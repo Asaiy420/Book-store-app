87241e7452e4839653cb6408f2d95b8c99fbdfb5
Asaiy420 <kcaryan05@gmail.com>
2025-04-01 12:34:15 +0545

    added cron module

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,14 @@ import authRoutes from "./routes/authRoutes.js";
 import { connectDB } from "../src/lib/db.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import cors from "cors";
+import job from "./lib/cron.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+job.start();
 app.use(express.json());
 app.use(cors());
 
