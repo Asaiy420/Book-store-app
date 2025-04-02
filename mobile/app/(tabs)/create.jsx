8f2ac5fec20939fb1d08320d392d75ad9cad1587
Asaiy420 <kcaryan05@gmail.com>
2025-04-02 13:39:39 +0545

    added UI in create tab

--- mobile/app/(tabs)/create.jsx
+++ mobile/app/(tabs)/create.jsx
@@ -1,10 +1,71 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TextInput,
+} from "react-native";
+import React, { useState } from "react";
+import { useRouter } from "expo-router";
+import styles from "../../assets/styles/create.styles";
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from "../../constants/colors";
 
 export default function Create() {
+  const [title, setTitle] = useState("");
+  const [caption, setCaption] = useState("");
+  const [rating, setRating] = useState(3);
+  const [image, setImage] = useState(null); //* displays the selected image
+  const [imageBase64, setImageBase64] = useState(null); //* translates the images to text
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const pickImage = async () => {};
+  const handleSubmit = async () => {};
+
   return (
-    <View>
-      <Text>Create tab</Text>
-    </View>
-  )
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        contentContainerStyle={styles.container}
+        style={styles.scrollViewStyle}
+      >
+        <View style={styles.card}>
+          {/* HEADER */}
+          <View style={styles.header}>
+            <Text style={styles.title}>Add book recommendation</Text>
+            <Text style={styles.subtitle}>
+              Post and share your favorite books with the world
+            </Text>
+          </View>
+          <View style={styles.form}>
+            {/* BOOK TITLE */}
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Book Title</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="book-outline"
+                  size={20}
+                  color={COLORS.textSecondary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter book title"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={title}
+                  onChangeText={setTitle}
+                />
+              </View>
+            </View>
+            {/* BOOK RATING */}
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
 }
